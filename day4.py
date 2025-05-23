0c58841c678e6ea65411fd200a5e1c07bb19c1d7
JJ Castillo <johnjeromecastillo@gmail.com>
2025-05-23 14:48:56 +0800

    Updated Fourth Commit

--- day4.py
+++ day4.py
@@ -1,25 +1,18 @@
 import cv2
 
-# Load the image
 image = cv2.imread('sample.jpg')
 
-# Check if image was loaded properly
 if image is None:
     print("Error: Image not found or path is incorrect.")
 else:
-    # Resize the image to a smaller fixed size: 320x240
     resized_image = cv2.resize(image, (620, 440))
 
-    # Convert to grayscale
     gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
-    # Display the resized original and grayscale images
     cv2.imshow('Small Original Image', resized_image)
     cv2.imshow('Small Grayscale Image', gray_image)
 
-    # Save the smaller grayscale image
     cv2.imwrite('gray_sample.jpg', gray_image)
 
-    # Wait for a key press and close all windows
     cv2.waitKey(0)
     cv2.destroyAllWindows()
